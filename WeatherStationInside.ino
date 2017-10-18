e14dd1b70fc8e3b8d0305707dd8e3935dd69adc4
BasMeijer <mail@basmeijer.com>
2017-10-18 20:29:38 +0200

    second version of the inside weather station code

--- WeatherStationInside.ino
+++ WeatherStationInside.ino
@@ -1,19 +1,22 @@
-// LIBRARIES IMPORTS
+// ----------------------------------------------------
+// LIB IMPORTS
+// ----------------------------------------------------
+#include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "MQ135.h"
 
-// PIN DEFINITIONS
+// ----------------------------------------------------
+// PIN & SENSOR DEFINITIONS 
+// ----------------------------------------------------
 
-// GAS SENSOR DEFINITIONS
+// GAS SENSOR
 #define ANALOGPIN A0    //  Define Analog PIN on Arduino Board
 #define RZERO 169.18
-float ppm;
 MQ135 gasSensor = MQ135(ANALOGPIN);
 
 // OBJECT DETECTION
@@ -29,52 +32,134 @@ Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
+// ----------------------------------------------------
+// DATA VARIABLES
+// ----------------------------------------------------
+
+// SENSOR READINGS
+float humidity;
+float temperature;
+float airpressure;
+float pressureAltitude;
+String raining;
+float ppm;
+float airspeed;
+
+// ----------------------------------------------------
+// WIFI & NETWORK SETTINGS
+// ----------------------------------------------------
 
 HTTPClient http;
-
-//const char *ssid = "#";
-//const char *password = "#";
 byte ledPin = 2;
-char ssid[] = "#";               // SSID of your home WiFi
-char password[] = "#";               // password of your home WiFi
+char ssid[] = "PinguHotspot";               // SSID of your home WiFi
+char password[] = "basbas1337";               // password of your home WiFi
 
 String httpPostUrl = "http://iot-open-server.herokuapp.com/data";
-String apiToken = "#";
+String apiToken = "b69d453c30c82a48619472ac";
+
+WiFiServer server(80);
+IPAddress ip(192, 168, 43, 198);            // IP address of the server
+IPAddress gateway(192, 168, 0, 1);        // gateway of your network
+IPAddress subnet(255, 255, 255, 0);       // subnet mask of your network
+
+unsigned long clientTimer = 0;
 
 
-
-WiFiServer server(80);                    
-IPAddress ip(192, 168, 0, 80);            // IP address of the server
-IPAddress gateway(192,168,2,254);           // gateway of your network
-IPAddress subnet(255,255,255,0);          // subnet mask of your network
-
+// ----------------------------------------------------
+// HOMESTATION STARTUP FUNCTIONS
+// ----------------------------------------------------
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Running Setup...");
+  
   pinMode(IR, INPUT); 
-  setupWebServer();
-    
-//  connectToWifiNetwork();
+  
+  connectToWifiNetwork();
+  
+  delay(5000);
 }
+
+
+// ----------------------------------------------------
+// MAIN LOOP FUNCTIONALITY
+// ----------------------------------------------------
 
 void loop() {
 
   receiveClientMessages();
   
-  float rzero = gasSensor.getRZero();
-  Serial.print("MQ135 RZERO Calibration Value : ");
-  Serial.println(rzero);
+  detection = digitalRead(IR);
+  
+  if(detection == LOW){
+    showDisplay(ppm);
+  }
 
-  ppm = gasSensor.getPPM();
-  digitalWrite(13,HIGH);
-  Serial.print("CO2 ppm value : ");
-  Serial.println(ppm);
+  if (millis() - clientTimer > 30000) {    // stops and restarts the WiFi server after 30 sec
+    Serial.println("Restarting Server");
+    WiFi.disconnect();                     // idle time
+    delay(5000);
+    server_start(1);
+  }
+  
+  delay(1000);
+}
 
-  StaticJsonBuffer<200> jsonBuffer;
+
+// ----------------------------------------------------
+// MAIN FUNCTIONS
+// ----------------------------------------------------
+
+void receiveClientMessages(){
+  WiFiClient client = server.available();
+  if (client) {
+    if (client.connected()) {
+      digitalWrite(LED_BUILTIN, LOW);  // to show the communication only (inverted logic)
+      Serial.println(".");            // Dot Dot try connecting
+      String request = client.readStringUntil('\r');    // receives the message from the client
+
+      ppm = gasSensor.getPPM();
+      String humidityval = getValue(request, ';', 0);
+      String temperatureval = getValue(request, ';', 1);
+      String airpressureval = getValue(request, ';', 2);
+      String rainingval = getValue(request, ';', 3);
+      String airspeedval = getValue(request, ';', 4);
+
+      humidity = humidityval.toFloat();
+      temperature = temperatureval.toFloat();
+      airpressure = airpressureval.toFloat();
+      airspeed = airspeedval.toFloat();
+
+      if(rainingval.equals("1")){
+        raining = "Raining";
+      }else if(rainingval.equals("0")){
+        raining = "Dry";
+      }
+
+      Serial.println("------ RECEIVED DATA -----");
+      Serial.print("From client: "); Serial.println(request);
+      Serial.println(humidityval);
+      Serial.println(temperatureval);
+      Serial.println(airpressureval);
+      Serial.println(rainingval);
+      Serial.println(airspeedval);
+      Serial.println("---------------------------");
+
+      client.flush();
+    }
+    client.stop();                // tarminates the connection with the client
+    clientTimer = millis();
+    
+    String dataToSend = createDataString();
+    Serial.println(dataToSend);
+    postData(dataToSend);
+  }  
+}
+
+
+String createDataString(){
+  StaticJsonBuffer<600> jsonBuffer;
   JsonObject& jsonRoot = jsonBuffer.createObject();
   
   jsonRoot["token"] = apiToken;
@@ -84,60 +169,44 @@ void loop() {
   JsonObject& airqualityObject = jsonBuffer.createObject();
   airqualityObject["key"] = "ppm";
   airqualityObject["value"] = ppm;
+
+  JsonObject& humidityObject = jsonBuffer.createObject();
+  humidityObject["key"] = "humidity";
+  humidityObject["value"] = humidity;
+
+  JsonObject& temperatureObject = jsonBuffer.createObject();
+  temperatureObject["key"] = "temperature";
+  temperatureObject["value"] = temperature;
+
+  JsonObject& airpressureObject = jsonBuffer.createObject();
+  airpressureObject["key"] = "airpressure";
+  airpressureObject["value"] = airpressure;
+
+  JsonObject& rainingObject = jsonBuffer.createObject();
+  rainingObject["key"] = "raining";
+  rainingObject["value"] = raining;
+
+  JsonObject& airspeedObject = jsonBuffer.createObject();
+  airspeedObject["key"] = "airspeed";
+  airspeedObject["value"] = airspeed;
   
   data.add(airqualityObject);
+  data.add(humidityObject);
+  data.add(temperatureObject);
+  data.add(airpressureObject);
+  data.add(rainingObject);
+  data.add(airspeedObject);
 
   String dataToSend;
   jsonRoot.printTo(dataToSend);
 
-  Serial.println(dataToSend);
-  
-  detection = digitalRead(IR);
-  if(detection == LOW){
-    showDisplay(ppm);
-  }
-  
-//  postData(dataToSend);
-  delay(500);
+  return dataToSend;
 }
 
-void connectToWifiNetwork() {
-  Serial.println ( "Trying to establish WiFi connection" );
-  WiFi.begin ( ssid, password );
-  Serial.println ( "" );
-  
-  // Wait for connection
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
 
-  Serial.println ( "" );
-  Serial.print ( "Connected to " );
-  Serial.println ( ssid );
-  Serial.print ( "IP address: " );
-  Serial.println ( WiFi.localIP() );
-
-//  if ( MDNS.begin ( "esp8266" ) ) {
-//    Serial.println ( "MDNS responder started" );
-//  }
-}
-
-void postData(String stringToPost) {
-  if(WiFi.status()== WL_CONNECTED){
-    http.begin(httpPostUrl);
-    http.addHeader("Content-Type", "application/json");
-    int httpCode = http.POST(stringToPost);
-    String payload = http.getString();
-  
-    Serial.println(httpCode);
-    Serial.println(payload);
-    http.end();
-  } else {
-    Serial.println("Wifi connection failed, retrying.");   
-    connectToWifiNetwork();
-  }
-}
+// ----------------------------------------------------
+// LCD DISPLAY FUNCTIONS
+// ----------------------------------------------------
 
 void showDisplay(float ppm){
     // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
@@ -158,8 +227,9 @@ void showDisplay(float ppm){
 
   display.setTextSize(1.5);
   display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(ppm);
+  display.println(temperature);
 
+  display.setCursor(0,15);
   display.setTextColor(WHITE);
   display.println("-------------");
 
@@ -169,47 +239,84 @@ void showDisplay(float ppm){
 
   display.setTextSize(1);
   display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.println(String(ppm));
-  
+  display.println(String(humidity));
   
   display.display();
-  delay(10000);
+  delay(5000);
   display.clearDisplay();
   display.display();
 }
 
-void setupWebServer(){
-  WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
-  WiFi.begin(ssid, password);                 // connects to the WiFi router
+
+
+// ----------------------------------------------------
+// WIFI & NETWORK FUNCTIONS
+// ----------------------------------------------------
+
+void server_start(byte restart) {
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
     delay(500);
   }
-  server.begin();                         // starts the server
-  Serial.println("Connected to wifi");
-  Serial.print("Status: "); Serial.println(WiFi.status());  // some parameters from the network
-  Serial.print("IP: ");     Serial.println(WiFi.localIP());
-  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
-  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
-  Serial.print("SSID: "); Serial.println(WiFi.SSID());
-  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
-  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());
-  pinMode(ledPin, OUTPUT);
+  server.begin();
+  delay(500);
+  clientTimer = millis();
 }
 
-void receiveClientMessages(){
-  WiFiClient client = server.available();
-  if (client) {
-    if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
-      Serial.println(".");
-      String request = client.readStringUntil('\r');    // receives the message from the client
-      Serial.print("From client: "); Serial.println(request);
-      client.flush();
-      client.println("Hi client! No, I am listening.\r"); // sends the answer to the client
-      digitalWrite(ledPin, HIGH);
-    }
-    client.stop();                // tarminates the connection with the client
+void connectToWifiNetwork() {
+  Serial.println ( "Trying to establish WiFi connection" );
+  WiFi.begin ( ssid, password );
+  Serial.println ( "" );
+  
+  // Wait for connection
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
   }
+
+  Serial.println ( "" );
+  Serial.print ( "Connected to " );
+  Serial.println ( ssid );
+  Serial.print ( "IP address: " );
+  Serial.println ( WiFi.localIP() );
+}
+
+void postData(String stringToPost) {
+  if (WiFi.status() == WL_CONNECTED) {
+    http.begin(httpPostUrl);
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(stringToPost);
+    String payload = http.getString();
+
+    Serial.println(httpCode);
+    Serial.println(payload);
+    Serial.println("POST DATA SUCCESFUL!");
+    
+    http.end();
+  } else {
+    Serial.println("Wifi connection failed, retrying.");
+    connectToWifiNetwork();
+  }
+}
+
+// ----------------------------------------------------
+// HELPER FUNCTIONS
+// ----------------------------------------------------
+
+// Gets the values from a String with seperated data
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
